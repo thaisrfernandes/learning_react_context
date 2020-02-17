7ca8515cd6817f77db0d1abc1d8d92407b4f6936
thaisrfernandes <thais.fernandes@ilegra.com>
2020-02-17 11:06:48 -0300

    base

--- context-app/src/App.js
+++ context-app/src/App.js
@@ -1,23 +1,12 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import BookList from "./components/BookList";
-import ThemeContextProvider from "./contexts/ThemeContext";
-import ThemeToggle from "./components/ThemeToggle";
-import AuthContextProvider from "./contexts/AuthContext";
 import BookContextProvider from "./contexts/BookContext";
 
 export const App = () => {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Navbar />
-          <BookContextProvider>
-            <BookList />
-          </BookContextProvider>
-          <ThemeToggle />
-        </AuthContextProvider>
-      </ThemeContextProvider>
+      <BookContextProvider>
+        
+      </BookContextProvider>
     </div>
   );
 };
--- context-app/src/contexts/BookContext.js
+++ context-app/src/contexts/BookContext.js
@@ -1,18 +1,25 @@
 import React, { createContext, useState } from "react";
-
+import uuid from "uuid/v1";
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
   const [books, setBooks] = useState([
-    { title: "name of the wind", id: 1 },
-    { title: "the way of kings", id: 2 },
-    { title: "the final empire", id: 3 },
-    { title: "the hero of ages", id: 4 }
+    { title: "name of the wind", author: "patrick rothfuss", id: 1 },
+    { title: "the final empire", author: "brandon sanderson", id: 2 }
   ]);
+
+  const addBook = (title, author) => {
+    setBooks([...books, { title, author, id: uuid() }]);
+  };
+
+  const removeBook = id => {
+    setBooks([books.filter(book => book.id !== id)]);
+  };
+
   return (
-    <BookContext.Provider value={{ books }}>
+    <BookContextProvider value={{ books, addBook, removeBook }}>
       {props.children}
-    </BookContext.Provider>
+    </BookContextProvider>
   );
 };
 
